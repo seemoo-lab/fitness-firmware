6a455baa3129f7b7526ed07e224962ce031f9f35
Daniel Wegemer <dwegemer@seemoo.tu-darmstadt.de>
2017-04-25 16:25:05 +0200

    added first patch to circumvent authentication for bluetooth commands

--- patches/fitbit/1/demo/src/patch.c
+++ patches/fitbit/1/demo/src/patch.c
@@ -4,22 +4,16 @@
 #include <patcher.h>
 #include <wrapper.h>
 
-void *
-main() {
-    blinkenlights(1, 0, 0, 5);
-    return 0;
+int
+hook_check_auth() {
+    return 1;
 }
 
-__attribute__((at(0x8027A60, "", CHIP_VER_FITBIT, FW_VER_FITBIT)))
-BPatch(main, main);
-
-// Hook the call to wlc_ucode_write in wlc_ucode_download
-//__attribute__((at(0x44ED0, "", CHIP_VER_BCM43438, FW_VER_7_45_41_26_r640327)))
-//BLPatch(wlc_ucode_write_compressed, wlc_ucode_write_compressed);
-
-// Update the ucode length to become the length of the extracted ucode before compression
-//__attribute__((at(0x4E9BC, "", CHIP_VER_BCM43438, FW_VER_7_45_41_26_r640327)))
-//GenericPatch4(ucode_length, 0xCC28);
-
-//__attribute__((at(0x2654, "", CHIP_VER_BCM43438, FW_VER_7_45_41_26_r640327)))
-//GenericPatch4(hndrte_reclaim_0_end, 0x592a4);
+__attribute__((at(0x800C6C4, "", CHIP_VER_FITBIT, FW_VER_FITBIT)))
+BLPatch(hook_check_auth1, hook_check_auth);
+__attribute__((at(0x800C704, "", CHIP_VER_FITBIT, FW_VER_FITBIT)))
+BLPatch(hook_check_auth2, hook_check_auth);
+__attribute__((at(0x800CA50, "", CHIP_VER_FITBIT, FW_VER_FITBIT)))
+BLPatch(hook_check_auth3, hook_check_auth);
+__attribute__((at(0x8013102, "", CHIP_VER_FITBIT, FW_VER_FITBIT)))
+BLPatch(hook_check_auth4, hook_check_auth);
